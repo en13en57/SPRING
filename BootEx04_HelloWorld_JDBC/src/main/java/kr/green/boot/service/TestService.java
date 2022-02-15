5603e2c3003c1a2046de9eb6d71a6f2dcef2987b
Doo O <en13en57@gmail.com>
2022-02-15 21:33:42 +0900

    education

---
+++ BootEx04_HelloWorld_JDBC/src/main/java/kr/green/boot/service/TestService.java
@@ -0,0 +1,9 @@
+package kr.green.boot.service;
+
+import java.util.Date;
+
+public interface TestService {
+	String selectNow();
+	Date selectToday();
+
+}
---
+++ Ex05_BeanAutoWiringAnnotation/src/main/java/kr/green/core/vo/Ferari.java
@@ -0,0 +1,13 @@
+package kr.green.core.vo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ferari implements Car{
+
+	@Override
+	public String getCarName() {
+		return "페라리";
+	}
+
+}
